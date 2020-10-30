b1e7e5b3e4491ae57a1a4e61bdc3abdc093ff947
Quentin David <qdavid@lipn.univ-paris13.fr>
2020-10-30 12:12:03 +0100

    Update pour le reconfinement! Bon courage les ami-e-s!

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ Vous pourrez alors sélectionner ces profils et ne remplir que les informations 
 python3 -m virtualenv .venv --python=/usr/bin/python3
 
 # Installation des dépendances
-.venv/bin/pip install -r requirements 
+.venv/bin/pip install -r requirements.txt
 ```
 
 ## Utilisation
@@ -38,6 +38,50 @@ Puis suivez les instructions.
 ```
 
 L'attestation est ensuite disponible dans le fichier `output.pdf`.
+
+## Changements entre le premier confinement et le reconfinement de Novembre
+
+Deux motifs ont été ajouté : 
+- Accompagnement de personnes en situation de handicap
+- Accompagnement des enfants à l'école ou pour activité périscolaire
+
+Le texte de l'attestation à quelque peut changé. Cf la branche ancien-confinement pour retrouver l'ancienne attestation (input-page1.png)
+
+J'ai remarqué dans le code du gouv que le pdf possédait des metadata. Je ne sais pas si c'était le cas auparavant.
+Je n'ai pas encore ajouté les metadata du gouv.
+``` javascript
+  // set pdf metadata
+  pdfDoc.setTitle('COVID-19 - Déclaration de déplacement')
+  pdfDoc.setSubject('Attestation de déplacement dérogatoire')
+  pdfDoc.setKeywords([
+    'covid19',
+    'covid-19',
+    'attestation',
+    'déclaration',
+    'déplacement',
+    'officielle',
+    'gouvernement',
+  ])
+  pdfDoc.setProducer('DNUM/SDIT')
+  pdfDoc.setCreator('')
+  pdfDoc.setAuthor("Ministère de l'intérieur")
+```
+
+La police utilisée pour remplir le formulaire est Helvetica sur le générateur du gouvernement. J'ai gardé Arial pour ce programme, vous pouvez faire un fork et changer dans `generate_pdf.py`:
+
+``` python
+FONT = "Arial.ttf"
+SMALL_LETTER_FONT = "arial.ttf"
+# Deviendra
+FONT = "Helevetica.ttf"
+SMALL_LETTER_FONT = "helvetica.ttf"
+```
+
+## Modifications futures
+
+- Je vais rajouter dans le prompt une vérification pour "la ville où vous êtes actuellement".
+- J'aimerai aussi pouvoir ajouter un serveur mail afin de pouvoir envoyer directement l'output plutôt que de devoir se l'envoyer à chaque fois...
+- Rajouter les metadata
 
 
 ## CRÉDITS
--- generate_pdf.py
+++ generate_pdf.py
@@ -11,7 +11,21 @@ import argparse
 
 
 # Motifs
-# travail-courses-sante-famille-sport-judiciaire-missions
+# travail-courses-sante-famille-handicap-sport-judiciaire-missions-ecole
+
+FONT = "Arial.ttf"
+SMALL_LETTER_FONT = "arial.ttf"
+
+def load_font(font_size):
+  try:
+    image_font = ImageFont.truetype(FONT, font_size)
+    return image_font
+  except OSError:
+    try:
+      image_font = ImageFont.truetype(SMALL_LETTER_FONT, font_size)
+      return image_font
+    except OSError:
+      sys.exit(f"{FONT} n'est pas installé")
 
 def parse_args():
     parser = argparse.ArgumentParser(prog='main.py')
@@ -27,87 +41,47 @@ def parse_args():
     
     return parser.parse_args()
 
-# ---------------------------
-#  First Page (All fields to fill)
-# ---------------------------
 def draw_first_page_layout():
-    img = Image.open("input-page1.png")
-    img_array = np.array(img)
+  img = Image.open("input-page1.png")
+  return np.array(img)
 
-    # Erase fields
-    img_array[300:330, 250:] = 255
-    img_array[355:390, 250:] = 255
-    img_array[400:430, 185:] = 255
-    img_array[450:490, 270:] = 255
-    img_array[895:925, 155:180] = 255
-    img_array[635:660, 155:180] = 255
-
-    # Erase crosses
-    img_array[630:660, 155:185] = 255
-    img_array[735:765, 155:185] = 255
-    img_array[820:850, 155:185] = 255
-    img_array[895:925, 155:185] = 255
-    img_array[1010:1040, 155:185] = 255
-    img_array[1110:1140, 155:185] = 255
-    img_array[1185:1215, 155:185] = 255
-
-    # Erase Current city
-    img_array[1260:1300, 220:527] = 255
-
-    # Erase Current date
-    img_array[1316:1339, 190:319] = 255
-
-    # Erase Current time
-    img_array[1315:1339, 409:500] = 255
-
-    # Erase Current time under QR
-    img_array[1442:1453, 948:1078] = 255
-
-    # Erase QR
-    img_array[1217:1430, 800:1100] = 255
-
-    img = Image.fromarray(img_array)
-    return img_array
 
 # Create crosses:
 def get_cross():
     image = Image.new('RGB', (30, 30), color=(255, 255, 255))
     image_draw = ImageDraw.Draw(image)
-    try:
-        image_font = ImageFont.truetype("Arial.ttf", 35)
-    except OSError:
-        try:
-            image_font = ImageFont.truetype("arial.ttf", 35)
-        except OSError:
-            sys.exit("arial.ttf n'est pas installé")
-    image_draw.text((3, -4), f'X', (0, 0, 0), font=image_font)
+    image_font = load_font(35)
+    image_draw.text((3, -4), f'x', (0, 0, 0), font=image_font)
     return np.array(image)
 
-
-# travail-courses-sante-famille-sport-judiciaire-missions
+# travail-courses-sante-famille-handicap-sport-judiciaire-missions-ecole
 def check_motif_boxes(img_array, motifs):
-    #img_array = np.array(first_page_img)
-    
     cross = get_cross()
     if "travail" in motifs:
-        img_array[630:660, 155:185] = cross
+        img_array[523:553, 169:199] = cross
     if "courses" in motifs:
-        img_array[735:765, 155:185] = cross
+        img_array[617:647, 169:199] = cross
     if "sante" in motifs:
-        img_array[820:850, 155:185] = cross
+        img_array[732:762, 169:199] = cross
     if "famille" in motifs:
-        img_array[895:925, 155:185] = cross
+        img_array[817:847, 169:199] = cross
+    if "handicap" in motifs:
+        img_array[904:934, 169:199] = cross
     if "sport" in motifs:
-        img_array[1010:1040, 155:185] = cross
+        img_array[982:1012, 169:199] = cross
     if "judiciaire" in motifs:
-        img_array[1110:1140, 155:185] = cross
+        img_array[1114:1144, 169:199] = cross
     if "missions" in motifs:
-        img_array[1185:1215, 155:185] = cross
+        img_array[1195:1225, 169:199] = cross
+    if "ecole" in motifs:
+        img_array[1291:1321, 169:199] = cross
     return img_array
+
 
 # QR CODE
 def draw_QR_code(img_array,first_name, last_name, birth_date, birth_city, address, leave_date, leave_hour, motifs):
     # Pas de diacritiques, est-ce normal ? (alors qu'il peut y en avoir dans les cases à remplir)
+    # Oui, c'est comme c'est qu'est conçu le QR dans les fichiers sources du repo du gouv
     qr_text = f"Cree le: {datetime.datetime.now().strftime('%d/%m/%Y a %H:%M')};" \
               f" Nom: {last_name};" \
               f" Prenom: {first_name};" \
@@ -116,7 +90,6 @@ def draw_QR_code(img_array,first_name, last_name, birth_date, birth_city, addres
               f" Sortie: {leave_date} a {leave_hour};" \
               f" Motifs: {motifs}"
 
-    # qr_text="hyduzqhdzoiqd zqoihdpodqz"
     qr = qrcode.make(qr_text, border=0)
     qr = qr.resize((200, 200))
     qr = np.array(qr).astype(np.uint8) * 255
@@ -124,34 +97,28 @@ def draw_QR_code(img_array,first_name, last_name, birth_date, birth_city, addres
     
     return qr
 
-
 # Fill args
 def fill_save_first_page(img_array, qr, first_name, last_name, birth_date, birth_city, current_city, address):
     # Draw QR code
-    img_array[1228:1428, 890:1090] = np.array(qr)
+    img_array[1344:1544, 910:1110] = np.array(qr)
     
     img = Image.fromarray(img_array)
 
     draw = ImageDraw.Draw(img)
-    try:
-        font = ImageFont.truetype("Arial.ttf", 22)
-        font_small = ImageFont.truetype("Arial.ttf", 14)
-    except OSError:
-        try:
-            font = ImageFont.truetype("arial.ttf", 22)
-            font_small = ImageFont.truetype("arial.ttf", 14)
-        except OSError:
-            sys.exit("arial.ttf n'est pas installé")
-    draw.text((260, 307), f'{first_name} {last_name}', (0, 0, 0), font=font)
-    draw.text((255, 357), f'{birth_date}', (0, 0, 0), font=font)
-    draw.text((190, 407), f"{birth_city}", (0, 0, 0), font=font)
-    draw.text((280, 458), f"{address}", (0, 0, 0), font=font)
+    font = load_font(22)
+    font_small = load_font(14)
 
-    draw.text((228, 1268), f"{current_city}", (0, 0, 0), font=font)
-    draw.text((190, 1319), datetime.datetime.now().strftime("%d/%m/%Y"), (0, 0, 0), font=font)
-    draw.text((411, 1318), datetime.datetime.now().strftime("%H:%M"), (0, 0, 0), font=font)
+    draw.text((250, 282), f'{first_name} {last_name}', (0, 0, 0), font=font)
+    draw.text((250, 328), f'{birth_date}', (0, 0, 0), font=font)
+    draw.text((630, 328), f"{birth_city}", (0, 0, 0), font=font)
+    draw.text((280, 374), f"{address}", (0, 0, 0), font=font)
 
-    draw.text((948, 1443), datetime.datetime.now().strftime("%d/%m/%Y à %H:%M"), (0, 0, 0), font=font_small)
+    draw.text((228, 1368), f"{current_city}", (0, 0, 0), font=font)
+    draw.text((205, 1416), datetime.datetime.now().strftime("%d/%m/%Y"), (0, 0, 0), font=font)
+    draw.text((535, 1416), datetime.datetime.now().strftime("%H:%M"), (0, 0, 0), font=font)
+
+    # No more in reconfinement
+    # draw.text((948, 1443), datetime.datetime.now().strftime("%d/%m/%Y à %H:%M"), (0, 0, 0), font=font_small)
 
     plt.imsave("output-1.pdf", np.array(img), format="pdf")
 
@@ -191,4 +158,18 @@ if __name__ == '__main__':
     args = parse_args()
     generate_pdf(args.first_name, args.last_name, args.birth_date, args.birth_city, args.current_city, args.address, args.leave_date, args.leave_hour, args.motifs)
 
+
+def fake_generation():
+  motifs = ['travail', 'courses', 'sante', 'famille', 'handicap', 'sport', 'judiciaire', 'missions', 'ecole']
+  first_name = "AAAAAAA"
+  last_name = "BBBBBBBB"
+  birth_date = "11/20/2000"
+  birth_city = "BIRTHCITYYYYYYYYYYYYYYYY"
+  current_city = "CURRENTCITYYYYYYYYYYY"
+  address = "1112 PLACE DE LARUE QUI EST LONGUE 12312 LONGUEVIIIIIIIIILLE"
+
+  leave_date = "11/11/1111"
+  leave_hour = "12h12"
+
+  generate_pdf(first_name, last_name, birth_date, birth_city, current_city, address, leave_date, leave_hour, motifs)
 
